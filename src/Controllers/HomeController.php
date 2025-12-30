6ea543f72e0c5ad5ad1e60d2c148670d59862fda
gonanx <xgonan99@gmail.com>
2025-12-30 16:05:58 +0100

    hasta_hoy

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Core\Controller;
+
+class HomeController extends Controller
+{
+    public function index(): void
+    {
+
+        $this->view('home/index', [
+            'titulo' => 'Home',
+            'mensaje' => 'Bienvenido al proyecto MVC 🚀'
+        ]);
+    }
+
+
+}
